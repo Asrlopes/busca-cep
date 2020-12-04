b044dcb28563ac35f6c2922d0221249fd408df16
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-04 16:47:01 -0400

    Add: Body container

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from '~/components/Header';
 
-import { Container } from './styles';
+import { Container, Body } from './styles';
 
 function Main() {
   return (
     <Container>
       <Header />
+      <Body>
+        <input type="text" />
+      </Body>
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,3 +4,11 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
 `;
+
+export const Body = styled.div`
+  height: 80%;
+  background-color: white;
+  margin: 1.875rem;
+  padding: 1.875rem;
+  box-shadow: 0rem 0rem 0.625rem 0.0625rem #000000;
+`;
