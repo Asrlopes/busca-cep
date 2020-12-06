3cdde9c15139da9e1baefe16e4ed27e71c7d6ac6
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-06 04:41:20 -0400

    add: cep finder

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import { ToastProvider } from './Toast';
+import { CepProvider } from './CepFinder';
 
-const AppProvider = ({ children }) => <ToastProvider>{children}</ToastProvider>;
+const AppProvider = ({ children }) => (
+  <ToastProvider>
+    <CepProvider>{children}</CepProvider>
+  </ToastProvider>
+);
 
 export default AppProvider;
