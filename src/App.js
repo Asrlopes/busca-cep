ad0f54cc33979baaf52109e6b994d2e11aa8ed85
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-06 00:39:43 -0400

    Add: AppProvider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import GlobalStyle from '~/styles/global';
 import Main from '~/pages/Main';
 
+import AppProvider from './hooks';
+
 function App() {
   return (
-    <>
+    <AppProvider>
       <GlobalStyle />
       <Main />
-    </>
+    </AppProvider>
   );
 }
 
