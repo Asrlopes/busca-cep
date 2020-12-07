e8678e88c40ce690e05d6e8b3b4867198120c9ba
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-07 11:27:01 -0400

    add: error handling

--- src/hooks/CepFinder.js
+++ src/hooks/CepFinder.js
@@ -8,18 +8,18 @@ const CepContext = createContext({});
 const CepProvider = ({ children }) => {
   const { addToast } = useToast();
 
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [item, setItem] = useState(null);
+  const [emptyData, setEmptyData] = useState(false);
 
   const FindCepByCode = useCallback(async ({ code }) => {
     setLoading(true);
 
     try {
-      const response = await api.get(`${code}/jsson/`);
+      const response = await api.get(`${code}/json/`);
 
       const responseData = response.data;
-
-      console.log(responseData, 'a');
 
       if (responseData.erro) {
         addToast({
@@ -27,9 +27,15 @@ const CepProvider = ({ children }) => {
           title: 'Erro na busca',
           description: 'Cep inexistente',
         });
+
+        setEmptyData(true);
+        setLoading(false);
+        setData(null);
+        return;
       }
 
       setData(response.data.length ? responseData : [responseData]);
+      setEmptyData(false);
       setLoading(false);
     } catch (error) {
       addToast({
@@ -38,12 +44,84 @@ const CepProvider = ({ children }) => {
         description:
           'Não foi possivel solicitar o cep, por favor verifique sua conexão com a internet',
       });
+      setEmptyData(false);
+      setData(null);
       setLoading(false);
     }
   }, []);
 
+  const FindCepByUfCityAddress = useCallback(async ({ uf, city, address }) => {
+    if (address && address.length < 3) {
+      addToast({
+        type: 'error',
+        title: 'Erro na busca',
+        description: 'O logradouro deve ter no mínimo 3 caracteres',
+      });
+      return;
+    }
+
+    if (!city) {
+      addToast({
+        type: 'error',
+        title: 'Erro na busca',
+        description: 'Por favor informe a cidade',
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await api.get(`${uf}/${city}/${address}/json/`);
+
+      const responseData = response.data;
+
+      if (responseData.length === 0) {
+        addToast({
+          type: 'error',
+          title: 'Erro na busca',
+          description:
+            'Verifique o formato uf cidade e caso necessário adicione um logradouro',
+        });
+
+        setEmptyData(true);
+        setLoading(false);
+        setData(null);
+        return;
+      }
+
+      setData(response.data.length ? responseData : [responseData]);
+      setEmptyData(false);
+      setLoading(false);
+    } catch (error) {
+      addToast({
+        type: 'error',
+        title: 'Erro na busca',
+        description:
+          'Não foi possivel solicitar o cep, por favor verifique sua conexão com a internet',
+      });
+      setEmptyData(false);
+      setData(null);
+      setLoading(false);
+    }
+  }, []);
+
+  const StoreCepData = ({ cepData }) => {
+    setItem(cepData);
+  };
+
   return (
-    <CepContext.Provider value={{ data, FindCepByCode, loading }}>
+    <CepContext.Provider
+      value={{
+        data,
+        FindCepByCode,
+        FindCepByUfCityAddress,
+        loading,
+        StoreCepData,
+        item,
+        emptyData,
+      }}
+    >
       {children}
     </CepContext.Provider>
   );
