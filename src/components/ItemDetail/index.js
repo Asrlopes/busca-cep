7eacff370687dc2b6b59dbc0cf3d218546144bd8
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-07 11:25:37 -0400

    add: item detail component

---
+++ src/components/ItemDetail/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+
+import { Container } from './styles';
+import { useCep } from '~/hooks/CepFinder';
+
+function ItemDetail() {
+  const { item } = useCep();
+
+  useEffect(() => {
+    console.log(item);
+  }, [item]);
+
+  return (
+    <Container>
+      {item && (
+        <>
+          <h3>Informações Complementares</h3>
+          <span>
+            Cidade: {item.localidade}, {item.uf}
+          </span>
+          <span>Cep: {item.cep}</span>
+          {item.bairro && <span>Bairro: {item.bairro}</span>}
+          {item.logradouro && <span>Logradouro: {item.logradouro}</span>}
+          {item.complemento && <span>Complemento: {item.complemento}</span>}
+          <span>DDD: {item.ddd}</span>
+        </>
+      )}
+    </Container>
+  );
+}
+
+export default ItemDetail;
---
+++ src/components/ItemDetail/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 0.4;
+
+  padding: 0.9375rem;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+
+  span {
+    font-size: 1.2rem;
+    padding: 0.625rem;
+  }
+`;
