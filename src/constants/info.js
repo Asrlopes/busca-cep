7e9dae4e46e2b7cc1a7ed2923d7a5bce78dda867
Amorésio de Souza <a.junior.srlopes@gmail.com>
2020-12-07 11:26:10 -0400

    add: infoType constant

---
+++ src/constants/info.js
@@ -0,0 +1,9 @@
+const InfoType = {
+  CEP: 'É necessário 8 digitos para formar um cep. Exemplo: 00000-000',
+  UF:
+    'Informe a UF, seguido da cidade. Exemplo: UF Cidade. Caso não encontre a cidade será necessário informar o logradouro.',
+  logradouro:
+    'Informe o logradouro para uma busca completa, no mínimo 3 caracteres.',
+};
+
+export default InfoType;
